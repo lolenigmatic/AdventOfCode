ee22459666302e0bceda23c73c5de3975ab536a4
lolenigmatic <quanabcis2107@gmail.com>
2021-12-01 11:40:49 +0000

    Initial commit

---
+++ src/com/company/Day1.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Day1 {
+    public int solve(File input) throws FileNotFoundException {
+        int result;
+        Scanner scanner = new Scanner(input);
+
+        while(scanner.hasNext()){
+
+        }
+
+        return result;
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.io.File;
+
+public class Main {
+
+    public static void main(String[] args) {
+        File file = new File("Day1input.txt");
+        Day1 task = new Day1();
+
+
+    }
+}
