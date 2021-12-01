69c2cc347ad9888882b7944de47b0e25f6e52744
lolenigmatic <quanabcis2107@gmail.com>
2021-12-01 12:17:03 +0000

    Day 1 tasks 1 and 2

--- src/com/company/Day1.java
+++ src/com/company/Day1.java
@@ -2,17 +2,50 @@ package com.company;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Day1 {
-    public int solve(File input) throws FileNotFoundException {
-        int result;
+    public int solve1(File input) throws FileNotFoundException {
         Scanner scanner = new Scanner(input);
+        int count = -1;
+        int prevNumber = 0;
+        int currentNumber;
 
         while(scanner.hasNext()){
+            currentNumber = scanner.nextInt();
 
+            if(prevNumber < currentNumber){
+                count++;
+            }
+
+            prevNumber = currentNumber;
         }
 
-        return result;
+        return count;
+    }
+
+    public int solve2(File input) throws FileNotFoundException{
+        Scanner scanner = new Scanner(input);
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        while(scanner.hasNextInt()){
+            int number = scanner.nextInt();
+            numbers.add(number);
+        }
+
+        int count = -1;
+        int thisSum;
+        int prevSum = 0;
+
+        for (int i = 0; i < numbers.size() - 2; i++){
+            thisSum = numbers.get(i) + numbers.get(i + 1) +numbers.get(i + 2);
+            if(thisSum > prevSum){
+                count++;
+            }
+            prevSum = thisSum;
+        }
+
+        return count;
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,19 @@
 package com.company;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 
 public class Main {
 
     public static void main(String[] args) {
-        File file = new File("Day1input.txt");
+        File file = new File("./src/com/company/Day1Input.txt");
         Day1 task = new Day1();
 
+        try {
+            System.out.println(task.solve2(file));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
     }
 }
