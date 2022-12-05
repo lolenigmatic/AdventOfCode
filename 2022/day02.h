c8c390daa3ff26564e327b0753843858e8febd09
Quan Tran <quanabcis2003@gmail.com>
2022-12-05 00:13:59 +0000

    Remove comments

--- 2022/day02.h
+++ 2022/day02.h
@@ -36,31 +36,3 @@ int d2_p2(vector<string> file){
 
     return total;
 }
-
-/*
-x = lose
-y = tie
-z = win
-*/
-
-/*
-Same: 0 points
-1 - 1 = 0
-1 - 2 = -1 lose
-1 - 3 = -2 win
-2 - 1 = 1 win
-2 - 2 = 0 
-2 - 3 = -1 lose
-3 - 1 = 2 lose
-3 - 2 = 1 win
-3 - 3 = 0 
-
-x = me
-y = opponent
-
-convert all rocks to 1, scissors to 2, papers to 3
-total += num
-
-if((x + 2)%3 == y) then win 
-if((x+1)%3 == y) then lose
-else tie*/
