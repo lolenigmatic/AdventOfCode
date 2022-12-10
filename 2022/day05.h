5ccdf582f38c852e4477c1a0365edc356e8e2903
Quan Tran <quanabcis2003@gmail.com>
2022-12-10 01:59:52 +0000

    Formatting and more concise day6 solution

--- 2022/day05.h
+++ 2022/day05.h
@@ -13,40 +13,48 @@ move 1 from 2 to 1
 move 3 from 1 to 3
 move 2 from 2 to 1
 move 1 from 1 to 2
- 1   2   3 
+ 1   2   3
  */
-void move(int numToMove, int from, int to, vector<stack<char>> & stacks) {
-    for(int i = 0; i < numToMove; ++i){
-        stacks[to-1].push(stacks[from-1].top());
-        stacks[from-1].pop();
+void move(int numToMove, int from, int to, vector<stack<char>> &stacks)
+{
+    for (int i = 0; i < numToMove; ++i)
+    {
+        stacks[to - 1].push(stacks[from - 1].top());
+        stacks[from - 1].pop();
     }
 }
 
-void move_new(int numToMove, int from, int to, vector<stack<char>> & stacks) {
+void move_new(int numToMove, int from, int to, vector<stack<char>> &stacks)
+{
     vector<char> toMove;
 
-    for (int i = 0; i < numToMove; ++i) {
-        toMove.push_back(stacks[from-1].top());
-        stacks[from-1].pop();
+    for (int i = 0; i < numToMove; ++i)
+    {
+        toMove.push_back(stacks[from - 1].top());
+        stacks[from - 1].pop();
     }
     reverse(toMove.begin(), toMove.end());
 
-    for (auto c : toMove) {
-        stacks[to-1].push(c);
+    for (auto c : toMove)
+    {
+        stacks[to - 1].push(c);
     }
 }
 
-string d5_p1(vector<string> file) {
-    //Iterate through each column
+string d5_p1(vector<string> file)
+{
+    // Iterate through each column
     const int COLUMNS = 9;
     const int MAX_HEIGHT = 8;
     vector<stack<char>> stacks;
 
-    for(int i = 1; i < 1 + (4 * COLUMNS); i += 4){
+    for (int i = 1; i < 1 + (4 * COLUMNS); i += 4)
+    {
         int currentRow = MAX_HEIGHT - 1;
         stack<char> thisColumn;
 
-        while (currentRow != -1 && file[currentRow][i] != ' ') {
+        while (currentRow != -1 && file[currentRow][i] != ' ')
+        {
             thisColumn.push(file[currentRow][i]);
             currentRow--;
         }
@@ -54,32 +62,37 @@ string d5_p1(vector<string> file) {
         stacks.push_back(thisColumn);
     }
 
-    for (int i = MAX_HEIGHT+2; i < file.size(); ++i) {
+    for (int i = MAX_HEIGHT + 2; i < file.size(); ++i)
+    {
         vector<string> lineParse = split(file[i], " ");
-        
+
         move(stoi(lineParse[1]), stoi(lineParse[3]), stoi(lineParse[5]), stacks);
     }
 
     string resultString = "";
 
-    for (auto s : stacks){
+    for (auto s : stacks)
+    {
         resultString += s.top();
     }
 
     return resultString;
 }
 
-string d5_p2(vector<string> file) {
-    //Iterate through each column
+string d5_p2(vector<string> file)
+{
+    // Iterate through each column
     const int COLUMNS = 9;
     const int MAX_HEIGHT = 8;
     vector<stack<char>> stacks;
 
-    for(int i = 1; i < 1 + (4 * COLUMNS); i += 4){
+    for (int i = 1; i < 1 + (4 * COLUMNS); i += 4)
+    {
         int currentRow = MAX_HEIGHT - 1;
         stack<char> thisColumn;
 
-        while (currentRow != -1 && file[currentRow][i] != ' ') {
+        while (currentRow != -1 && file[currentRow][i] != ' ')
+        {
             thisColumn.push(file[currentRow][i]);
             currentRow--;
         }
@@ -87,15 +100,17 @@ string d5_p2(vector<string> file) {
         stacks.push_back(thisColumn);
     }
 
-    for (int i = MAX_HEIGHT+2; i < file.size(); ++i) {
+    for (int i = MAX_HEIGHT + 2; i < file.size(); ++i)
+    {
         vector<string> lineParse = split(file[i], " ");
-        
+
         move_new(stoi(lineParse[1]), stoi(lineParse[3]), stoi(lineParse[5]), stacks);
     }
 
     string resultString = "";
 
-    for (auto s : stacks){
+    for (auto s : stacks)
+    {
         resultString += s.top();
     }
 
--- 2022/day06.h
+++ 2022/day06.h
@@ -6,36 +6,31 @@
 #include "helpers.h"
 using namespace std;
 
-int d6_p1(vector<string> file){
-    int markerPos = 0;
-
-    for(int i = 0; i < file[0].length(); ++i){
+int d6_p1(vector<string> file)
+{
+    for (int i = 0; i < file[0].length(); ++i)
+    {
         string thisSubString = file[0].substr(i, 4);
         set<char> stringSet(thisSubString.begin(), thisSubString.end());
-        
+
         if (stringSet.size() == 4)
         {
-            markerPos = i + 4;
-            break;
+            return i + 4;
         }
     }
-
-    return markerPos;
 }
 
-int d6_p2(vector<string> file){
-    int markerPos = 0;
+int d6_p2(vector<string> file)
+{
 
-    for(int i = 0; i < file[0].length(); ++i){
+    for (int i = 0; i < file[0].length(); ++i)
+    {
         string thisSubString = file[0].substr(i, 14);
         set<char> stringSet(thisSubString.begin(), thisSubString.end());
-        
+
         if (stringSet.size() == 14)
         {
-            markerPos = i + 14;
-            break;
+            return i + 14;
         }
     }
-
-    return markerPos;
 }
